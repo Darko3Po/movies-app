29feb452c04b87bb4aaabd24f1a07e8ca7c35194
Darko3Po <potajacdarko9@gmail.com>
2024-01-03 15:05:02 +0100

    Added show page, route and method show

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -64,7 +64,14 @@ class MoviesController extends Controller
      */
     public function show($id)
     {
-        //
+        //  Use API endpoints for show movie
+        $movie= Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/'.$id)
+            ->json();
+
+        dump($movie);
+
+        return  view('show', compact('movie'));
     }
 
     /**
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,18 +3,20 @@
 @section('content')
     <div class="movie-info b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-            <img src="img/parasite.jpg" alt="parasite" class="w-64 md:w-96">
+            <img src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path']  }}" alt="parasite" class="w-64 md:w-96">
             <div class="md:ml-24">
-               <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
+               <h2 class="text-4xl font-semibold">{{ $movie['title']  }} </h2>
                 <div class="flex flex-wrap items-center text-gray-400">
                     <svg class="w-4 h-4 text-yellow-500 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                     </svg>
-                    <span class="ml-1">92%</span>
+                    <span class="ml-1">{{ round($movie['vote_average'], 1) * 10 . '%' }}</span>
                     <span class="mx-2">|</span>
-                    <span>Feb 20, 2020</span>
+                    <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('D M Y')  }}</span>
                     <span class="mx-2">|</span>
-                    <span class="mx-2">Action, Drama</span>
+                    <span class="mx-2">
+
+                    </span>
                 </div>
 
                 <p class="text-gray-300 mt-8">
