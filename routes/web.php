329345e8c2b22ebca10379dca126c3f809ea610c
Darko3Po <potajacdarko9@gmail.com>
2024-01-04 18:46:10 +0100

    Added logic on show page for actors section

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[MoviesController::class,'index'])->name('movies.index');
 Route::get('/movies/{movie}',[MoviesController::class,'show'])->name('movies.show');
 
+
+
