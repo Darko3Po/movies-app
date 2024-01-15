adb1fc03c0ea332ec191aafeaaf9e61e83b6682a
Darko3Po <potajacdarko9@gmail.com>
2024-01-15 17:55:31 +0100

    Update search dropdown

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
-    public $search = 'hello theree';
+    /*
+      * This os logic for Search, data use API search form themoviedborg
+      * */
+    public $search = '';
 
     public function render()
     {
-        return view('livewire.search-dropdown');
+
+        $searchResults = [];
+        /*
+         * Logic for the search frontend, where it outputs results based on the first 2 letters
+         * */
+        if (strlen($this->search)>2){
+            // A variable in which a series of files for search is placed
+            $searchResults = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
+                ->json()['results'];
+        }
+
+        $searchResults = array_slice($searchResults,0,7);
+
+        //dump($searchResults);
+
+        return view('livewire.search-dropdown', compact('searchResults'));
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -5,15 +5,20 @@
     </div>
     <div class="absolute bg-gray-800 rounded w-64 mt-4">
         <ul>
-            <li class="border-b border-gray-700 ">
-                <a href="#" class="block hover:bg-gray-700 px-3 py-3">{{ $search  }}</a>
-            </li>
-            <li class="border-b border-gray-700 ">
-                <a href="#" class="block hover:bg-gray-700 px-3 py-3">Avengers</a>
-            </li>
-            <li class="border-b border-gray-700 ">
-                <a href="#" class="block hover:bg-gray-700 px-3 py-3">Avengers</a>
-            </li>
+            {{--  Logic for the search frontend, where it outputs results based on the first 2 letters--}}
+            @foreach($searchResults as $result)
+                <li class="border-b border-gray-700 ">
+                    <a href="{{route('movies.show',$result['id'])}}"
+                       class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                        @if($result['poster_path'])
+                            <img src="https://image.tmdb.org/t/p/w92/{{$result['poster_path']}}" alt="portret" class="w-8">
+                        @else
+                            <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                        @endif
+                       <span class="ml-4"> {{ $result['title']  }}</span>
+                    </a>
+                </li>
+            @endforeach
         </ul>
     </div>
 </div>
